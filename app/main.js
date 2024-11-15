24143875a5a4d8688073bc315f99212cfb60ed0f
Aldacelio <aldacelio@alu.ufc.br>
2024-11-15 10:52:10 -0300

    chore: move function to separate file for better readability

--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
+import { renderLivros } from './exibirLivros.js';
+
 let livros = [];
 
 const endpointAPI =
   "https://guilhermeonrails.github.io/casadocodigo/livros.json";
-
-const livrosElemento = document.getElementById("livros");
 
 getLivros();
 
@@ -16,19 +16,4 @@ async function getLivros() {
   } catch (error) {
     console.log("Não conseguiu buscar os livros", error);
   }
-}
-
-function renderLivros(listaDeLivros) {
-  listaDeLivros.forEach((livro) => {
-    livrosElemento.innerHTML += `
-        <div class="livro">
-            <img class="livro__images" src="${livro.imagem}" alt="${livro.alt}" />
-            <h2 class="livro__titulo">${livro.titulo}</h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-            <div class="tags">
-                <span class="tag">${livro.categoria}</span>
-            </div>
-    `;
-  });
 }
